487a2bf2fd2f70114283a879003fae8ea66e2d4a
Filip Dobrinić <dobrinic.filip@gmail.com>
2021-03-26 11:06:03 +0100

    cube replication setup for both server and client

--- Source/NetworkSetupProject/MovingPlatform.h
+++ Source/NetworkSetupProject/MovingPlatform.h
@@ -17,6 +17,7 @@ class NETWORKSETUPPROJECT_API AMovingPlatform : public AStaticMeshActor
 public:
 	AMovingPlatform();
 
+	virtual void BeginPlay() override;
 	virtual void Tick(float deltaTime) override;
 
 	UPROPERTY(EditAnywhere)
