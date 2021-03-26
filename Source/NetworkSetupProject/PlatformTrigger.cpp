d12f16e0c6b94bb6bf03f84ec4aeb1725b9586df
Filip Dobrinić <dobrinic.filip@gmail.com>
2021-03-26 14:40:52 +0100

    mad modifications, platform triggers added

--- Source/NetworkSetupProject/PlatformTrigger.cpp
+++ Source/NetworkSetupProject/PlatformTrigger.cpp
@@ -21,6 +21,7 @@ void APlatformTrigger::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	TriggerVolume->OnComponentBeginOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapBegin);
 }
 
 // Called every frame
@@ -30,3 +31,12 @@ void APlatformTrigger::Tick(float DeltaTime)
 
 }
 
+void APlatformTrigger::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Activated"));
+}
+
+void APlatformTrigger::OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Deactivated"));
+}
