dd8827bd09dfa937b50a0eac6f246e606a0a61cc
Filip Dobrinić <dobrinic.filip@gmail.com>
2021-03-30 16:00:51 +0200

    main menu on click basic log implementation

--- Source/NetworkSetupProject/MainMenu.cpp
+++ Source/NetworkSetupProject/MainMenu.cpp
@@ -1,5 +1,31 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "MainMenu.h"
 
+bool UMainMenu::Initialize()
+{
+	bool initialized = Super::Initialize();
+	if (!initialized)
+	{
+		return false;
+	}
+	
+	if (!ensure(HostButton != nullptr) || !ensure(JoinButton != nullptr))
+	{
+		return false;
+	}
+
+	HostButton->OnClicked.AddDynamic(this, &UMainMenu::OnHostButtonClicked);
+	JoinButton->OnClicked.AddDynamic(this, &UMainMenu::OnJoinButtonClicked);
+	return true;
+}
+
+void UMainMenu::OnHostButtonClicked()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Hosting server."));
+}
+
+void UMainMenu::OnJoinButtonClicked()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Joining server."));
+}
--- Source/NetworkSetupProject/MainMenu.h
+++ Source/NetworkSetupProject/MainMenu.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/Button.h"
 #include "MainMenu.generated.h"
 
 /**
@@ -14,7 +15,20 @@ class NETWORKSETUPPROJECT_API UMainMenu : public UUserWidget
 {
 	GENERATED_BODY()
 	
-public:
-	//UPROPERTY(meta = (BindWidget))
+protected:
+	virtual bool Initialize() override;
 
+public:
+	UFUNCTION()
+	void OnHostButtonClicked();
+
+	UFUNCTION()
+	void OnJoinButtonClicked();
+
+protected:
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* HostButton;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* JoinButton;
 };
